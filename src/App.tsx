807a672698b0b45628d2c23471d42c6b49e2e70a
Sanjeev Kumar <premchandrasingh786@gmail.com>
2025-06-20 12:10:10 +0530

    Portfolio Website

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import NavBar from "./components/layout/NavBar";
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { HiHome, HiFolder, HiInformationCircle, HiMail } from "react-icons/hi";
+import "../../styles/navbar.css";
+import type { ReactElement } from "react";
+
+const NavBar = () => {
+  const [activeSection, setActiveSection] = useState<string>("");
+  const handleScroll = () => {
+    const sections = document.querySelectorAll("section");
+    let currentSection = "home";
+
+    // Find the section that's most visible in the viewport
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      const sectionId = section.getAttribute("id");
+
+      // Check if section is past the middle of the screen
+      if (rect.top <= window.innerHeight / 2) {
+        currentSection = sectionId || "home";
+      }
+    });
+
+    setActiveSection(currentSection);
+  };
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const links: { name: string; href: string; icons: ReactElement }[] = [
+    { name: "Home", href: "#home", icons: <HiHome /> },
+    { name: "Projects", href: "#projects", icons: <HiFolder /> },
+    { name: "About", href: "#about", icons: <HiInformationCircle /> },
+    { name: "Contact", href: "#contact", icons: <HiMail /> },
+  ];
+  return (
+    <div className="navbar-container">
+      <nav className="navbar">
+        <ul className="navbar-links">
+          {links.map((link) => (
+            <li key={link.name} className="navbar-item">
+              <a
+                className={`navbar-link ${
+                  activeSection === link.name.toLowerCase() ? "active" : ""
+                }`}
+                href={link.href}
+              >
+                <span className={`navbar-link-icon`}>{link.icons}</span>
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
